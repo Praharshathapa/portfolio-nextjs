1af2592a3467eca9ebdd0e4209ca203d1fec1554
Praharshathapa <106920069+Praharshathapa@users.noreply.github.com>
2023-09-20 14:43:58 +0545

    chnaged minor fixes

--- components/skills.jsx
+++ components/skills.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Skills = () => {
-    const skillsList = ["HTML", "CSS", "JavaScript", "React", "Node.js", "UI/UX Design"];
+    const skillsList = ["HTML", "CSS", "JavaScript", "React", "Node.js", "Figma", "Python", "Data Science:Extraction,Collection,Visualiazation,Manipulation"];
 
     return (
         <div className=" p-4 rounded shadow-md">
-            <h2 className="text-2xl font-bold mb-4">Skills</h2>
-            <ul className="flex flex-wrap">
+            <h2 className="text-3xl font-bold mb-4">Skills</h2>
+            <ul className="columns-1 text-s">
                 {skillsList.map((skill, index) => (
-                    <li key={index} className="mr-2 mb-2 p-2   ">
+                    <li key={index} className="mr-2 mb-2 p-2 text-gray-400 ">
                         {skill}
                     </li>
                 ))}

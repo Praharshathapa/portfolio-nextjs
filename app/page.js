ac91b4b38b5c0b08bff910cb72b9022129d1241b
Praharshathapa <106920069+Praharshathapa@users.noreply.github.com>
2023-09-24 11:39:36 +0545

    responsive for about section

--- app/page.js
+++ app/page.js
@@ -10,34 +10,38 @@ import ProjectList from '@/components/projects/Projects';
 import ContactForm from '@/components/contactinfo/Contactform';
 import "./page.css"
 import BackgroundAnimation from '../components/background/animat';
+import Hobby from '@/components/hobby';
 
 export default function Home() {
   return (
-    <div className='custom-cursor relative z-5'>
+    <div className='custom-cursor  z-5'>
       <div className="min-h-screen">
         <NavBar />
         <RenderList />
-        <section id="hi" className="h-screen flex justify-center items-center z-1">
+        <section id="hi" className="h-screen flex flex-col justify-center items-center z-1">
           <BackgroundAnimation />
 
-          <div className="profile-picture-container relative ml-4 w-60 h-60 overflow-hidden shadow-lg">
-            <Image
-              src={profile}
-              alt="Profile Picture"
-              objectFit="cover"
-              layout="fill"
-              className="profile-picture rounded-custom-shape"
-            />
-          </div>
-          <div className="text-container relative ml-4">
-            <h1 className="text-4xl font-bold mb-2 glow-text">Hi! I am Praharsha Thapa</h1>
-            <div className="w-20 h-1 bg-cyan-600 rounded-full mb-4"></div>
-            <p className="text-xl font-semibold">Welcome to my portfolio</p>
+          <div className="profile-container flex flex-col md:flex-row items-center ml-4">
+            <div className="profile-picture-container w-60 h-60 md:w-96 md:h-96 overflow-hidden shadow-lg">
+              <Image
+                src={profile}
+                alt="Profile Picture"
+                objectFit="cover"
+                layout="fill"
+                className="profile-picture rounded-custom-shape"
+              />
+            </div>
+            <div className="text-container relative ml-4 text-center md:text-left">
+              <h1 className="mb-2 glow-text">Hi! I am Praharsha Thapa</h1>
+              <div className="w-20 h-1 bg-cyan-600 rounded-full mb-4"></div>
+              <p className="text-2xl font-semibold">Welcome to my portfolio</p>
+            </div>
           </div>
         </section>
 
-        <div className="flex flex-col mx-auto mr-10 md:flex-row mt-10 max-w-screen-xl">
-          <div className="md:w-1/2 p-4">
+
+        <div className="flex flex-col mx-auto mr-10 md:flex-row  max-w-screen-xl">
+          <div className="md:w-1/2 p-4 ">
             <Education />
           </div>
           <div className="md:w-1/2 p-4">
@@ -49,7 +53,7 @@ export default function Home() {
           <ProjectList />
         </section>
         <section id="Hobby" className="text-white h-screen bg-black text-center flex justify-center items-center">
-          <h1 className="text-6xl font-bold text-cyan-600">Hobby</h1>
+          <Hobby />
         </section>
         <section id="Contacts" className="text-white h-screen bg-cyan-700 flex justify-center items-center">
           <ContactForm />
@@ -58,4 +62,3 @@ export default function Home() {
     </div>
   );
 }
-

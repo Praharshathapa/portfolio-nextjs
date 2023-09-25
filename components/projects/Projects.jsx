792d82338cdf05f0307792af1d544fdd6308d32a
Praharshathapa <106920069+Praharshathapa@users.noreply.github.com>
2023-09-25 08:24:29 +0545

    using map added the projects but no image

--- components/projects/Projects.jsx
+++ components/projects/Projects.jsx
@@ -19,7 +19,7 @@ const ProjectList = () => {
         },
     ];
 
-    const [selectedProject, setSelectedProject] = useState(null);
+    const [selectedProject, setSelectedProject] = useState(False);
 
     const openDescription = (projectId) => {
         setSelectedProject(projectId);
@@ -32,7 +32,33 @@ const ProjectList = () => {
     return (
         <div className="container mx-auto py-8">
             <h2 className="text-3xl font-semibold text-center mb-8 ">Projects</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {projects.map((project) => (
+                    <div
+                        key={project.id}
+                        className="bg-white p-4 rounded-lg shadow-md cursor-pointer transform hover:scale-105 transition-transform duration-300"
+                        onClick={() => openDialog(project.id)}
+                    >
+                        <img
+                            src={`${project.id}`}
+                            alt={project.title}
+                            className="w-full h-auto rounded-lg mb-4"
+                        />
+                        <h3 className="text-xl font-semibold mb-2 hover:text-cyan-600">{project.title}</h3>
+                        <p className="text-gray-600 mb-4">{project.description}</p>
+                        <a
+                            href={project.link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-cyan-600 hover:underline"
+                        >
+                            Visit Project
+                        </a>
+                    </div>
+                ))}
+            </div>
         </div>
+
     )
 }
 

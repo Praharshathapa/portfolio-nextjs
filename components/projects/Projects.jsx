3a8e793a58e244ab90a5bfa0eac785e0394769b0
Praharshathapa <106920069+Praharshathapa@users.noreply.github.com>
2023-09-25 08:18:18 +0545

    deleted sample code

--- components/projects/Projects.jsx
+++ components/projects/Projects.jsx
@@ -17,7 +17,6 @@ const ProjectList = () => {
             title: 'Project 2',
             description: 'This is the description of Project 2.',
         },
-        // Add more projects here
     ];
 
     const [selectedProject, setSelectedProject] = useState(null);
@@ -31,28 +30,6 @@ const ProjectList = () => {
     };
 
     return (
-        <div>
-            <h2>Projects</h2>
-            <ul>
-                {projects.map((project) => (
-                    <li key={project.id}>
-                        <button onClick={() => openDescription(project.id)}>
-                            {project.title}
-                        </button>
-                    </li>
-                ))}
-            </ul>
-            {selectedProject !== null && (
-                <div className="project-popup">
-                    <div className="project-popup-content">
-                        <h3>{projects[selectedProject - 1].title}</h3>
-                        <p>{projects[selectedProject - 1].description}</p>
-                        <button onClick={closeDescription}>Close</button>
-                    </div>
-                </div>
-            )}
-        </div>
-    );
-};
+        
 
 export default ProjectList;

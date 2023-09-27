304ad571c130945168e615ad74bccab4d7c17a98
Praharshathapa <106920069+Praharshathapa@users.noreply.github.com>
2023-09-27 19:07:55 +0545

    changed styling and structure

--- components/contactinfo/Contactform.jsx
+++ components/contactinfo/Contactform.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Instagram, Linkedin, GitHub, Twitter } from 'react-feather';
 import { motion } from 'framer-motion';
+
 function ContactForm() {
     const socialMediaLinks = [
         { name: 'Instagram', icon: <Instagram />, url: 'https://www.instagram.com/your_username' },
@@ -8,11 +9,11 @@ function ContactForm() {
         { name: 'GitHub', icon: <GitHub />, url: 'https://github.com/your_username' },
         { name: 'Twitter', icon: <Twitter />, url: 'https://twitter.com/your_handle' },
     ];
-    
+
     const iconVariants = {
         hover: { scale: 1.2 }, 
     };
-    
+
     const [formData, setFormData] = useState({
         email: '',
         phone: '',
@@ -32,7 +33,6 @@ function ContactForm() {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-
         e.target.reset();
 
         setFormData({
@@ -47,10 +47,41 @@ function ContactForm() {
     return (
         <div className="min-h-screen bg-black container mx-auto py-10 text-white flex flex-col lg:flex-row">
             <div className="lg:w-1/2 p-8">
-                
+                <h2 className="text-4xl md:text-6xl font-semibold text-center mb-6 text-cyan-600">Contact Me</h2>
+                <p className="text-cyan-600 mb-6 text-center">
+                    <a
+                        href="mailto:thapapraharsha@gmail.com"
+                        className="text-cyan-200 hover:text-cyan-100 transition duration-300 ease-in-out text-lg"
+                    >
+                        thapapraharsha@gmail.com
+                    </a>
+                </p>
+                <div className="flex justify-center">
+                    <ul className="flex">
+                        {socialMediaLinks.map((link, index) => (
+                            <motion.li
+                                key={index}
+                                className="mr-3"
+                                initial="initial"
+                                whileHover="hover"
+                                variants={iconVariants}
+                            >
+                                <a
+                                    href={link.url}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-cyan-500 hover:text-cyan-100 transition duration-300 ease-in-out"
+                                >
+                                    {link.icon}
+                                </a>
+                            </motion.li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+            <div className="lg:w-1/2 p-8">
                 <div className="bg-gray-300 p-8 rounded-lg shadow-lg">
                     <form onSubmit={handleSubmit}>
-                        {/* Email Input */}
                         <div className="mb-4">
                             <label htmlFor="frm-email" className="text-cyan-800 font-medium block mb-2">Email</label>
                             <input
@@ -65,7 +96,6 @@ function ContactForm() {
                             />
                         </div>
 
-                        {/* Phone Input */}
                         <div className="mb-4">
                             <label htmlFor="frm-phone" className="text-cyan-800 font-medium block mb-2">Phone</label>
                             <input
@@ -80,9 +110,8 @@ function ContactForm() {
                             />
                         </div>
 
-                        {/* Name Inputs */}
-                        <div className="mb-4 flex">
-                            <div className="w-1/2 pr-2">
+                        <div className="mb-4 flex flex-col lg:flex-row">
+                            <div className="lg:w-1/2 pr-2">
                                 <label htmlFor="frm-first" className="text-cyan-800 font-medium block mb-2">First Name</label>
                                 <input
                                     id="frm-first"
@@ -95,7 +124,7 @@ function ContactForm() {
                                     onChange={handleInputChange}
                                 />
                             </div>
-                            <div className="w-1/2 pl-2">
+                            <div className="lg:w-1/2 pl-2">
                                 <label htmlFor="frm-last" className="text-cyan-800 font-medium block mb-2">Last Name</label>
                                 <input
                                     id="frm-last"
@@ -127,18 +156,6 @@ function ContactForm() {
                         </div>
                     </form>
                 </div>
-            </div>
-            <div className="lg:w-1/2 p-8">
-                <h2 className="text-6xl font-semibold text-center mb-6 text-cyan-600">Contact Me</h2>
-                <p className="text-cyan-600 mb-6 text-center">
-                    <a
-                        href="mailto:thapapraharsha@gmail.com"
-                        className="text-cyan-200 hover:text-cyan-100 transition duration-300 ease-in-out text-xl"
-                    >
-                        thapapraharsha@gmail.com
-                    </a>
-                </p>
-               
             </div>
         </div>
     );
